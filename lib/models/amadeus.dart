cb2f0f62b306e5e1e4b35623939d2e19d26e7bf1
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2023-07-05 01:32:21 +0200

    Create token url in env and update generateAccessToken

--- lib/models/amadeus.dart
+++ lib/models/amadeus.dart
@@ -7,19 +7,25 @@ class Amadeus {
   String? token;
 
   Future<String?> generateAccessToken() async {
+    final String tokenUrl = dotenv.env['TOKEN_URL'] as String;
     final String clientId = dotenv.env['CLIENT_ID'] as String;
     final String clientSecret = dotenv.env['CLIENT_SECRET'] as String;
-    final Uri authorizationUri =
-        Uri.parse("https://test.api.amadeus.com/v1/security/oauth2/token");
-    Response? response;
+    final Uri authorizationUrl = Uri.parse(tokenUrl);
+    late final Response response;
 
     try {
-      response = await post(authorizationUri,
-          headers: {"Content-type": "application/x-www-form-urlencoded"},
-          body:
-              "grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret");
+      response = await post(
+        authorizationUrl,
+        headers: {"Content-type": "application/x-www-form-urlencoded"},
+        body: {
+          'grant_type': 'client_credentials',
+          'client_id': clientId,
+          'client_secret': clientSecret
+        },
+      );
     } catch (e) {
       log(e.toString());
+
       return null;
     }
     final Map data = jsonDecode(response.body);
