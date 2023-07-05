f0239986dd947534dec141390f31a6254d6b0aff
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2023-07-06 00:56:47 +0200

    Update code and create deletePlace

--- lib/blocs/place_cubit.dart
+++ lib/blocs/place_cubit.dart
@@ -1,34 +1,30 @@
 import 'dart:developer';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_journey_diary/blocs/place_state.dart';
+import 'package:flutter_journey_diary/models/place.dart';
 import 'package:flutter_journey_diary/repositories/place_repository.dart';
-
-import '../models/place.dart';
 
 class PlaceCubit extends Cubit<PlaceState> {
   PlaceCubit(this.placeRepository) : super(PlaceState.loading());
 
   final PlaceRepository placeRepository;
-  List<Place> listPlace = [];
 
-  Future<void> savePlace(Place place) async{
-    await placeRepository.savePlace(place);
-    emit(PlaceState.loaded(listPlace));
-  }
-
-  Future<void> getPlaces() async{
-    try{
+  Future<void> getPlaces() async {
+    try {
       emit(PlaceState.loading());
-      listPlace = await placeRepository.getPlaces();
-      emit(PlaceState.loaded(listPlace));
-      print(listPlace.length);
-    }
-    catch (e) {
+      final List<Place> places = await placeRepository.getPlaces();
+      emit(PlaceState.loaded(places));
+    } catch (e) {
       log(e.toString());
       emit(PlaceState.error());
     }
-
   }
 
+  Future<void> savePlace(Place place) async {
+    await placeRepository.savePlace(place);
+  }
+
+  Future<void> deletePlace(Place place) async {
+    await placeRepository.deletePlace(place);
+  }
 }
