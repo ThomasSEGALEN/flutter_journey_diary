4cc4d83e192b51a21dcfe92e2b750cb17cee8729
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2023-07-05 14:02:49 +0200

    Add globals styles

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,8 @@ import 'package:flutter_journey_diary/repositories/user_repository.dart';
 import 'package:flutter_journey_diary/ui/screens/home_page.dart';
 import 'package:flutter_journey_diary/ui/screens/login_page.dart';
 import 'package:flutter_journey_diary/ui/shared/colors.dart';
+import 'package:flutter_journey_diary/ui/shared/fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Future<void> main() async {
   await dotenv.load();
@@ -60,10 +62,78 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Journey Diary',
       theme: ThemeData(
+        textTheme: TextTheme(
+          bodySmall: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.regular,
+            fontSize: JDFontSize.bodySmall,
+          ),
+          bodyMedium: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.regular,
+            fontSize: JDFontSize.bodyMedium,
+          ),
+          bodyLarge: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.regular,
+            fontSize: JDFontSize.bodyLarge,
+          ),
+          labelSmall: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.medium,
+            fontSize: JDFontSize.labelSmall,
+          ),
+          labelMedium: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.medium,
+            fontSize: JDFontSize.labelMedium,
+          ),
+          labelLarge: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.medium,
+            fontSize: JDFontSize.labelLarge,
+          ),
+          titleSmall: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.medium,
+            fontSize: JDFontSize.titleSmall,
+          ),
+          titleMedium: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.medium,
+            fontSize: JDFontSize.titleMedium,
+          ),
+          titleLarge: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.medium,
+            fontSize: JDFontSize.titleLarge,
+          ),
+          headlineSmall: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.semiBold,
+            fontSize: JDFontSize.headlineSmall,
+          ),
+          headlineMedium: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.semiBold,
+            fontSize: JDFontSize.headlineMedium,
+          ),
+          headlineLarge: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.semiBold,
+            fontSize: JDFontSize.headlineLarge,
+          ),
+          displaySmall: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.bold,
+            fontSize: JDFontSize.displaySmall,
+          ),
+          displayMedium: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.bold,
+            fontSize: JDFontSize.displayMedium,
+          ),
+          displayLarge: GoogleFonts.poppins(
+            fontWeight: JDFontWeight.bold,
+            fontSize: JDFontSize.displayLarge,
+          ),
+        ),
         textSelectionTheme: TextSelectionThemeData(
           cursorColor: Colors.grey,
           selectionColor: Colors.grey.shade300,
-          selectionHandleColor: const Color(JourneyColor.congoPink),
+          selectionHandleColor: const Color(JDColor.congoPink),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor:
+                MaterialStateProperty.resolveWith((state) => Colors.black),
+          ),
         ),
       ),
       home: BlocBuilder<UserCubit, bool>(
