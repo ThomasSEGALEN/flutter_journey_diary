117e1b960f9a5f3e4807e3624d5d655d05099275
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2023-07-06 13:40:11 +0200

    Clean location repo

--- lib/repositories/location_repository.dart
+++ lib/repositories/location_repository.dart
@@ -1,6 +1,1 @@
-import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-class LocationRepository {
-  final Dio dio = Dio(BaseOptions(baseUrl: dotenv.env['BASE_URL'] as String));
-}
+class LocationRepository {}
